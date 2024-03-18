e7bd66ce144468698b45969c54d5950ab87e884a
ellie489 <114486930+ellie489@users.noreply.github.com>
2024-03-18 20:57:17 +0100

    Adds Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About</li>
+                <li>Another thing</li>
+            </ul>
+        </header>
+    );
+}
